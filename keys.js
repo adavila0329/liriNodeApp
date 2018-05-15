fe1f862004adea056ccb8bfc69e70574da4018f2
Alan Davila <alandavila@Alans-MacBook-Pro.local>
2018-05-15 00:06:38 -0500

    added files and code

---
+++ keys.js
@@ -0,0 +1,14 @@
+// js
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.vs6ppl6BGZKExbxyGkBdjLUHq,
+  consumer_secret: process.env.jYGEC5pibfRpUtFr9SpQvYhrPeUJ5AeKjOZWugADnTCo2VjEAp,
+  access_token_key: process.env.OurErxhkMN1ylUOAwGNC10H6qlpvCv5,
+  access_token_secret: process.env.eY1GNtInAeEqkYGWdjFYXvE2XZDs63Jyk2RpmrTHGAFk3
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
